cf025b524e71238bb79de80fc730789d909dcc0b
Yoel Buzgalo (RIT Student) <ybb5956@rit.edu>
2023-11-17 10:52:37 -0500

    Activity 11.3

--- pets.py
+++ pets.py
@@ -1,4 +1,5 @@
 class Pet:
+    CALORIES_PER_POUND = 3000
     __slots__ = ['species', 'name', 'weight', 'fur_color', 'age']
     def __init__(self,species, name, weight, fur_color, age=0):
         self.species = species
@@ -6,3 +7,7 @@ class Pet:
         self.weight = weight
         self.fur_color = fur_color
         self.age = age
+
+    def feed(self, calories):
+        pounds = calories / Pet.CALORIES_PER_POUND
+        self.weight += pounds
--- pets_main.py
+++ pets_main.py
@@ -3,6 +3,8 @@ import pets
 def main():
     dog = pets.Pet('Golden Retriever', 'Bucky', 50, 'Gold', 4)
     print(dog.name, dog.weight)
+    dog.feed(1800)
+    print(dog.weight)
 
 if __name__ == "__main__":
     main()
